b2fa6cf6fa6fd8ba3ce78f7a1d2cfd5552cdd0de
qiyulongadmin <3300945610@qq.com>
2020-04-02 15:09:31 +0800

    在添加管理员时验证用户名是否重复

--- resources/views/admin/manager/create.blade.php
+++ resources/views/admin/manager/create.blade.php
@@ -7,6 +7,7 @@
 		<title>普通网站管理系统</title>
         {{--<meta name="csrf-token" content="{{ csrf_token() }}">--}}
 		<meta name="description" content="overview &amp; stats" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         @include('admin.public.style')
     </head>
@@ -79,7 +80,7 @@
                                                                         <i class="icon-leaf"></i>
                                                                     </span>
                                                                 </div>
-                                                                <div class="help-block col-xs-12 col-sm-reset inline red">
+                                                                <div id="xiaoxi" class="help-block col-xs-12 col-sm-reset inline red">
                                                                     *(必填)
                                                                 </div>
                                                             </div>
@@ -88,7 +89,7 @@
 
                                                                 <div class="col-xs-12 col-sm-5 inline help-block">
                                                                     <span class="block input-icon input-icon-right">
-                                                                        <input type="text" name="Manage[real_name]"  id="name" class="width-100">
+                                                                        <input type="text" name="Manage[real_name]"  id="real_name" class="width-100">
                                                                         <i class="icon-leaf"></i>
                                                                     </span>
                                                                 </div>
@@ -170,6 +171,88 @@
             @include('admin.public.footer')
 		</div>
         @include('admin/public/script')
+        <script>
+            var oldxiaoxi;
+            var oldclass;
+            //姓名获取焦点时显示，内含失去焦点显示点击前样式及内容
+            $("#name").focus(function() {
+                oldxiaoxi = $("#xiaoxi").text();
+                oldclass = $("#xiaoxi").attr("class");
+                //修改css样式为黑色
+                $("#xiaoxi").attr("class","help-block col-xs-12 col-sm-reset inline");
+                //修改HTML内容
+                $("#xiaoxi").html("（正在输入...）");
+                //当点击完输入框不做操作离开时避免显示正在输入，因此显示点击前所显示的内容及样式
+                // $("#name").blur(function() {
+                //     //修改css内容
+                //     $("#xiaoxi").attr("class",oldclass);
+                //     //修改HTML内容
+                //     $("#xiaoxi").html(oldxiaoxi);
+                // });
+            });
+
+            var oldxiaoxi1;
+            var oldclass1;
+            //键盘离开时显示，内含失去焦点显示键盘离开后样式及内容
+            $("#name").keyup(function() {
+                var name = $(" #name ").val();
+                //调用ajax
+                $.ajax({
+                    //请求方式
+                   type:"post",
+
+                   //服务器相应数据的解析方式
+                   dataType:"json",
+                   headers: {
+                           'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                       },
+                   //请求资源url
+                   url:"/admin/manager",
+                   //此处需要同步处理，即ajax执行完后才能向下进行。（避免第一次点击输入框只有一次键盘触发时oldxiaoxi为（正在输入...））
+                   // async: false,
+                   //向服务器端发送的数据
+                   data:{"name":name,"ajax":'cunzai'},
+                   success:function(data){
+                        $("#xiaoxi").attr("class",data.newclass);
+                        $("#xiaoxi").html(data.cont);
+                        oldxiaoxi1 = $("#xiaoxi").text();
+                        oldclass1 = $("#xiaoxi").attr("class");
+                    }
+                });
+            });
+
+            //失去焦点时的两种情况，1.获取焦点->失去焦点，2.获取焦点->键盘离开->失去焦点（最后一个else之后便是）
+            $("#name").blur(function() {
+
+                if(oldxiaoxi1==="(用户名已存在)"){
+                    $("#name").val("");
+                    //修改css内容
+                    $("#xiaoxi").attr("class","help-block col-xs-12 col-sm-reset inline red");
+                    //修改HTML内容
+                    $("#xiaoxi").html("*(必填)");
+                }else if(oldxiaoxi1==="(用户名可用)"){
+                    // 修改css内容
+                    $("#xiaoxi").attr("class",oldclass1);
+                    //修改HTML内容
+                    $("#xiaoxi").html(oldxiaoxi1);
+                }else{
+                    //修改css内容
+                    $("#xiaoxi").attr("class",oldclass);
+                    //修改HTML内容
+                    $("#xiaoxi").html(oldxiaoxi);
+                }
+
+            });
+
+
+        </script>
+
+
+
+
+
+
+
         {{--<script>
             //监听提交
             from.on('subit(add)',function(date)){
